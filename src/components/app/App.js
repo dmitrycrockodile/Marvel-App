6c6f4b310f318f487b459c6e488048764b256b7b
Dmitrij <dmitry.bermas@gmail.com>
2022-07-20 16:43:32 +0200

    prepared project for publishing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -18,9 +18,9 @@ const App = () => {
                 <main>
                     <Suspense fallback={<Spinner/>}> 
                         <Routes> 
-                            <Route path="/Marvel-App/" element={<MainPage />} />
-                            <Route path="/Marvel-App/comics" element={<ComicsPage />}/>
-                            <Route path="/Marvel-App/comics/:comicId" element={<SingleComicPage />}/>
+                            <Route path="https://dmitrycrockodile.github.io/Marvel-App/" element={<MainPage />} />
+                            <Route path="https://dmitrycrockodile.github.io/Marvel-App/comics" element={<ComicsPage />}/>
+                            <Route path="https://dmitrycrockodile.github.io/Marvel-App/:comicId" element={<SingleComicPage />}/>
                             <Route path="*" element={<Page404 />}/>
                         </Routes>
                     </Suspense>
