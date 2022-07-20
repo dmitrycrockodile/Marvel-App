bfef9e3b6737d1c657e9d64e53a94fe544d68540
Dmitrij <dmitry.bermas@gmail.com>
2022-07-20 16:28:44 +0200

    prepared project for publishing

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -18,9 +18,9 @@ const App = () => {
                 <main>
                     <Suspense fallback={<Spinner/>}> 
                         <Routes> 
-                            <Route path="/" element={<MainPage />} />
-                            <Route path="/comics" element={<ComicsPage />}/>
-                            <Route path="/comics/:comicId" element={<SingleComicPage />}/>
+                            <Route path="/Marvel-App/" element={<MainPage />} />
+                            <Route path="/Marvel-App/comics" element={<ComicsPage />}/>
+                            <Route path="/Marvel-App/comics/:comicId" element={<SingleComicPage />}/>
                             <Route path="*" element={<Page404 />}/>
                         </Routes>
                     </Suspense>
