0e03307ef397617e0f36152bc5a72ee1a0517441
Dmitrij <dmitry.bermas@gmail.com>
2022-11-23 13:43:53 +0100

    Optimized CEO using react-helmet to provide different meta tags for different pages

--- src/components/pages/ComicsPage.js
+++ src/components/pages/ComicsPage.js
@@ -1,3 +1,5 @@
+import Helmet from "react-helmet";
+
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 import ComicsList from "../../components/comicsList/ComicsList";
 import AppBanner from "../appBanner/AppBanner";
@@ -5,6 +7,13 @@ import AppBanner from "../appBanner/AppBanner";
 const ComicsPage = () => {
    return (
       <>
+         <Helmet>
+            <meta
+               name="description"
+               content="Page with list of our comics"
+            />
+            <title>Comics page</title>
+         </Helmet>
          <AppBanner />
          <ErrorBoundary>
             <ComicsList />
